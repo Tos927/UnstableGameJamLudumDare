80b7ac854d224c5f400bbfb13e482a11df4665e9
Thoma-fr <thomas.raulin.pro@gmail.com>
2021-10-02 20:37:21 +0200

    last

---
+++ UnstableGameJam/Assets/Scripts/PavNum.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class PavNum : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //int Btn
+    int var;
+
+    public void OnBtnClick(int var)
+    {
+        Debug.Log("oof");
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
