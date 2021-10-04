1372d412fc7031b4c99407ed3364485f0bb1180f
Tos927 <t.sautereau92@gmail.com>
2021-10-04 10:19:38 +0200

    Ajout des screen de pause et settings dans la scene de jeu

--- UnstableGameJam/Assets/Scripts/Teo/PauseMenu.cs
+++ UnstableGameJam/Assets/Scripts/Teo/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -54,7 +55,7 @@ public class PauseMenu : MonoBehaviour
     public void loadMainMenu()
     {
         Resume();
-        //SceneManager.LoadScene("MainMenu");
+        SceneManager.LoadScene("MainMenu");
         //AudioManager.instance.Play("BgMusic");
         //AudioManager.instance.Stop("MenuMusic");
     }
